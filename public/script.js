6c9e4754c79d1e7deb57eb1791bc70b6916b46c7
juanma1blue-beep <juanma1blue@gmail.com>
2026-06-15 00:42:48 +0200

    Actualizar script.js

--- public/script.js
+++ public/script.js
@@ -1,20 +1,19 @@
 const socket = io();
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
+const volumeRemote = document.getElementById('volumeRemote');
 let localStream;
 
-// --- FUNCIÓN: Generar código de sala ---
+// --- FUNCIÓN: Generar código ---
 function generateRandomRoomId() {
     return Math.random().toString(36).substring(2, 9);
 }
 
-// --- FUNCIÓN: Asignar ID y mostrarlo en la interfaz ---
+// --- FUNCIÓN: Asignar ID ---
 function setRoomId() {
     const roomInput = document.getElementById('roomInput');
     const displayDiv = document.getElementById('room-id-display');
     const newId = generateRandomRoomId();
-    
-    // Ponemos el ID en el input y en la cajita gris
     if (roomInput) roomInput.value = newId;
     if (displayDiv) displayDiv.innerText = "Tu código de sala: " + newId;
 }
@@ -28,11 +27,15 @@ async function startCamera() {
     }
 }
 
+// --- LOGICA VOLUMEN ---
+volumeRemote.addEventListener('input', (e) => {
+    remoteVideo.volume = e.target.value;
+});
+
 function joinRoom() {
     const roomId = document.getElementById('roomInput').value;
     if (roomId) {
         socket.emit('join-room', roomId);
-        console.log("Unido a la sala privada: " + roomId);
     } else {
         alert("Por favor, ingresa o genera un código de sala.");
     }
@@ -42,6 +45,5 @@ socket.on('user-connected', (userId) => {
     console.log("Usuario remoto conectado: " + userId);
 });
 
-// Iniciar cámara y generar ID al cargar la página
 startCamera();
 setRoomId();
